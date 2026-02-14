28b5bfb28df2a8fa9d3a97e9d9d3431da37aaaaf
Dhruvkulshrestha018 <dhruvkul2004@gmail.com>
2026-02-14 21:36:30 +0530

    CICD trigger

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -14,32 +14,32 @@ from src.logger import logging
 # Below code block is for production use
 # -------------------------------------------------------------------------------------
 # Set up DagsHub credentials for MLflow tracking
-# dagshub_token = os.getenv("CAPSTONE_TEST")
-# if not dagshub_token:
-#     raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
+dagshub_token = os.getenv("DAGSHUB_TOKEN")
+if not dagshub_token:
+    raise EnvironmentError("DAGSHUB_TOKEN environment variable is not set")
 
-# os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
-# os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
-# dagshub_url = "https://dagshub.com"
-# repo_owner = "vikashdas770"
-# repo_name = "YT-Capstone-Project"
+dagshub_url = "https://dagshub.com"
+repo_owner = "Dhruvkulshrestha018"
+repo_name = "MLOps-project"
 
-# # Set up MLflow tracking URI
-# mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
+# Set up MLflow tracking URI
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 # -------------------------------------------------------------------------------------
 
 # Below code block is for local use
 # -------------------------------------------------------------------------------------
-mlflow.set_tracking_uri('https://dagshub.com/Dhruvkulshrestha018/MLOps-project.mlflow')
-import os
-import dagshub
+# mlflow.set_tracking_uri('https://dagshub.com/Dhruvkulshrestha018/MLOps-project.mlflow')
+# import os
+# import dagshub
 
-dagshub.init(
-    repo_owner='Dhruvkulshrestha018',
-    repo_name='MLOps-project',
-    mlflow=True
-)
+# dagshub.init(
+#     repo_owner='Dhruvkulshrestha018',
+#     repo_name='MLOps-project',
+#     mlflow=True
+# )
 # -------------------------------------------------------------------------------------
 
 
